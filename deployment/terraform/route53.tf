a9f28987f0ba926456457ebfba82b7ce1095ca0a
Renjith Raj <renjithraj2005@gmail.com>
2026-05-06 18:02:59 +0530

    feat(terraform): add Route 53 A record for app.reviewbee.in

--- deployment/terraform/route53.tf
+++ deployment/terraform/route53.tf
@@ -26,3 +26,11 @@ resource "aws_route53_record" "www" {
   ttl     = 300
   records = [aws_eip.ec2.public_ip]
 }
+
+resource "aws_route53_record" "app" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "app.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.ec2.public_ip]
+}
